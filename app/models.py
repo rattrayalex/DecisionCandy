0726eb54b656ab78d838e0ec70c81bcd50cf304e
mouseplatterman <rattray.alex@gmail.com>
2011-08-17 18:18:49 -0400

    rewrote models
    did it with ceasar! better now.
    Signed-off-by: mouseplatterman <rattray.alex@gmail.com>

--- app/models.py
+++ app/models.py
@@ -1,43 +1,59 @@
 from django.db import models
-import random
+from django.contrib.auth.models import User
+import datetime
 
-
+gender_choices = (
+    ('M', 'Male'),
+    ('F', 'Female'),
+    )
 
 class Project(models.Model): 
     name = models.CharField(max_length=100)
     description = models.TextField()
-    creator = models.ForeignKey('Client')
+    creator = models.ForeignKey('Client', related_name='projects')
     reward = models.FloatField(default=0)
     criteria = models.CharField(max_length=100)
     more_criteria = models.TextField()
-    
-##  attributes = []
 
     def __unicode__(self):
         return self.name
 
 class Image(models.Model): 
-    project = models.ForeignKey(Project)
+    project = models.ForeignKey(Project, related_name='images')
     img = models.ImageField(upload_to='projects')
-    number = models.IntegerField(default=int(random.randrange(1,100,1)))
-    votes_for = models.IntegerField(default=0)
-    votes_against = models.IntegerField(default=0)
-    def __unicode__(self):
-        return str(self.project) + str(self.number)#image)
 
-class Client(models.Model): 
-    name = models.CharField(max_length=100)
+    def __unicode__(self):
+        return str(self.project) + str(self.id)
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=150)
     email = models.EmailField()
-    description = models.TextField()
+    user = models.OneToOneField(User, unique=True)
+    signup_date = models.DateTimeField(default=datetime.datetime.today())
+
+    class Meta:
+        abstract = True
+        
     def __unicode__(self):
         return self.name
 
+class Client(UserProfile):
+    description = models.TextField()
 
+class Minion(UserProfile):
+    gender = models.CharField(max_length=1,choices=gender_choices)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    city = models.CharField(max_length=30, blank=True)
+    state = models.CharField(max_length=2, blank=True)
+    country = models.CharField(max_length=50, blank=True)
 
+class Vote(models.Model):
+    winner = models.ForeignKey(Image, related_name='winvotes')
+    loser = models.ForeignKey(Image, related_name='losevotes')
+    voter = models.ForeignKey(Minion, null=True, blank=True)
+    index = models.IntegerField(null=True, blank=True)
+    time = models.DateTimeField(default=datetime.datetime.now())
 
-
-##def ImageHandler(instance, filename):
-##    project = 'fake1'
-##    path_base = 'projects/'
-##    path = path_base + project + '/' + filename
-##    return path
+    def __unicode__(self):
+        return "w: %s, l: %s" % (self.winner, self.loser)
+    
